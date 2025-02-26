c8e578fb88bafca0ae7419fbee4db2517c820433
Keileb-boop <125236416+Keileb-boop@users.noreply.github.com>
2025-02-25 18:05:44 -0600

    Delete Sistema-gestion-empresarial/Sistema-gestion-empresarial/src/test/java/sv/edu/udb/Java/Empleados/Contador.java

--- Sistema-gestion-empresarial/Sistema-gestion-empresarial/src/test/java/sv/edu/udb/Java/Empleados/Contador.java
+++
@@ -1,7 +0,0 @@
-package sv.edu.udb.Empleados;
-
-public class Contador extends Empleado {
-    public Contador(String nombre, int id, double salario) {
-        super(nombre, id, salario, "Finanzas");
-    }
-}
